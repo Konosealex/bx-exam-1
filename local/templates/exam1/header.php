fd96b513a0e999563edb1429a5dc6259addc7aca
Konovalov Sergey <konovalov@web-axioma.ru>
2020-04-16 15:13:11 +0700

    [ex1-7] Внедрить цепочку навигации

--- local/templates/exam1/header.php
+++ local/templates/exam1/header.php
@@ -98,13 +98,15 @@ IncludeTemplateLangFile(__FILE__);
     <!-- /nav -->
     <?if (!Csite::InDir('/index.php')):?>
     <!-- breadcrumbs -->
-    <div class="breadcrumbs-box">
-        <div class="inner-wrap">
-            <a href="">Главная</a>
-            <a href="">Мебель</a>
-            <span>Выставки и события</span>
-        </div>
-    </div>
+        <?$APPLICATION->IncludeComponent(
+            "bitrix:breadcrumb",
+            "breadcrumbs",
+            Array(
+                "PATH" => "",
+                "SITE_ID" => "s1",
+                "START_FROM" => "0"
+            )
+        );?>
     <!-- /breadcrumbs -->
     <?endif;?>
     <!-- page -->

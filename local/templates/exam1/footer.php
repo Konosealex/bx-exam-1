44ea94ddfde1349a425a39e6fc535c01b3f4d734
Konovalov Sergey <konovalov@web-axioma.ru>
2020-04-16 15:22:46 +0700

    [ex1-8] Внедрить меню: свой тип, нижнее меню

--- local/templates/exam1/footer.php
+++ local/templates/exam1/footer.php
@@ -109,14 +109,25 @@ IncludeTemplateLangFile(__FILE__);
         <nav class="main-menu">
             <div class="item">
                 <div class="title-block">О магазине</div>
-                <ul>
-                    <li><a href="">Отзывы</a>
-                    </li>
-                    <li><a href="">Руководство </a>
-                    </li>
-                    <li><a href="">История</a>
-                    </li>
-                </ul>
+                <?$APPLICATION->IncludeComponent(
+                    "bitrix:menu",
+                    "bottom",
+                    array(
+                        "COMPONENT_TEMPLATE" => "bottom",
+                        "ROOT_MENU_TYPE" => "bottom",
+                        "MENU_CACHE_TYPE" => "N",
+                        "MENU_CACHE_TIME" => "3600",
+                        "MENU_CACHE_USE_GROUPS" => "Y",
+                        "MENU_CACHE_GET_VARS" => array(
+                        ),
+                        "MAX_LEVEL" => "1",
+                        "CHILD_MENU_TYPE" => "bottom",
+                        "USE_EXT" => "N",
+                        "DELAY" => "N",
+                        "ALLOW_MULTI_SELECT" => "N"
+                    ),
+                    false
+                );?>
             </div>
             <div class="item">
                 <div class="title-block">Каталог товаров</div>
